a97de26dbe22ce9684b8ea2cc74410fb07bc6275
tan-weijie <tanweijie.91@gmail.com>
2022-01-10 10:59:18 +0800

    testing

--- test.js
+++ test.js
@@ -1,9 +1,19 @@
-import chai from "chai";
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('./index');
 
 const { expect } = chai;
 
-describe("Test", () => {
-  it("Basic Test", async () => {
-    expect(1).to.equal(1);
-  });
+chai.use(chaiHttp);
+
+describe("API routes", () => {
+  // GET route
+  it("/"), async () => {
+    chai.request(server)
+      .get("/api/user-1")
+      .end((err, response) => {
+        response.data.should.be.a('string');
+      })
+      .done();
+  }
 });
